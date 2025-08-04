14d9d3be9fe171cab61a408657edc9babc5874d8
55555555dsfg <molp95346@gmail.com>
2025-08-04 23:27:39 +0200

    UG.py

--- UG.py
+++ UG.py
@@ -1,12 +1,12 @@
 import random
 import string
 
-def generate_username(min_length=4, max_length=6):
+def generate_username(min_length=3, max_length=4):
     length = random.randint(min_length, max_length)
     characters = string.ascii_letters + string.digits
     return ''.join(random.choices(characters, k=length))
 
-def save_usernames(count=1000, filename="usernames.txt", min_length=6, max_length=6):
+def save_usernames(count=1000, filename="usernames.txt", min_length=3, max_length=4):
     with open(filename, "w") as file:
         file.writelines(f"{generate_username(min_length, max_length)}\n" for _ in range(count))
     print(f"Generated {count} usernames saved to {filename}")
